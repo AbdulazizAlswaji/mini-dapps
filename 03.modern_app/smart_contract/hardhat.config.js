e0319cc3d70cba50c3b472ba662ee4ab84b6d7d2
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-05-09 17:34:16 +0300

    adding ethereum

--- 03.modern_app/smart_contract/hardhat.config.js
+++ 03.modern_app/smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/otUXzcaIftq4OOMvYXQVyLjvdIBd1pW6',
-      
+      accounts:['c778d7d63b68c15a114705786bd544c58c4ce229bbd83f3928a81d09d4549367']
     }
   }
 }
