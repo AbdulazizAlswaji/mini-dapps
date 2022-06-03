b133b5536d94d730672d48b09510f4c55c045f64
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-06-03 14:49:14 +0300

    complete smart contract

--- 04.mint/app/contracts/RoboPunksNFT.sol
+++ 04.mint/app/contracts/RoboPunksNFT.sol
@@ -6,7 +6,7 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 
-contract RoboPunkNFT is ERC721, Ownable {
+contract RoboPunksNFT is ERC721, Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
@@ -14,7 +14,7 @@ contract RoboPunkNFT is ERC721, Ownable {
     bool public isPublicMintEnabled;
     string internal baseTokenUri;
     address payable public withdrawWallet;
-    mapping (address => uinit256) public walletMints;
+    mapping (address => uint256) public walletMints;
 
     constructor() payable ERC721('RoboPunks', 'RP') {
         mintPrice = 0.02 ether;
@@ -45,6 +45,19 @@ function withdraw() external onlyOwner {
     
 }
 
+function mint(uint256 quantity_) public payable {
+    require(isPublicMintEnabled, 'minting not enabled');
+    require(msg.value == quantity_ * mintPrice, 'minting not enabled');
+    require(totalSupply + quantity_ <= maxSupply, 'sold out');
+    require(walletMints[msg.sender] * quantity_ <= maxPerWallet , 'exceeded max wallet');
+
+    for (uint256 i = 0; i < quantity_; i++) {
+        uint256 newTokenId = totalSupply + 1;
+        totalSupply++;
+        _safeMint(msg.sender, newTokenId);
+
+    }
+}
 
 
 
