64c6d9c086ffd33a3ff6c8ce2b34cadef26b7d49
Abdulaziz Alswaji <abdulaziz.alswaji@gmail.com>
2022-06-27 20:25:50 +0300

    contract testing

--- 05.inbox/test/inbox.test.js
+++ 05.inbox/test/inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
@@ -23,9 +24,26 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
-    })
-})
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!')
+
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({
+            from: accounts[0]
+        });
+
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
+    });
+
+
+});
 
 
 
